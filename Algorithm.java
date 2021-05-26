d2e6b874b00ac4c067e4d780eb25cb22be649a33
LachTrip <lachlan.tripolone@students.mq.edu.au>
2021-05-26 10:27:55 +1000

    algorithm now prioritizes not "inactive" servers instead of "idle", using not inactive server with most cores.

--- Algorithm.java
+++ Algorithm.java
@@ -63,7 +63,48 @@ public class Algorithm {
 			} else {
 				next = s;
 				if (forUse.getCore() > next.getCore()){
-				// if ((forUse.getCore() > next.getCore()) || forUse.getCore() == next.getCore() && ((forUse.getWJobs() + forUse.getRJobs()) > (next.getWJobs() + next.getRJobs()))){
+					forUse = next;
+				}
+			}
+		}
+		for (Server s : servers){
+			next = s;
+			if ((forUse.getWJobs() + forUse.getRJobs() > 4) && forUse.getWJobs() + forUse.getRJobs() > next.getWJobs() + next.getRJobs()){
+				forUse = next;
+			}
+		}
+		for (Server s : servers){
+			next = s;
+			if (!next.getState().equals("inactive") && (forUse.getState().equals("inactive") || forUse.getCore() <= next.getCore())){
+				forUse = next;
+			}
+		}
+		send("OK");
+		return forUse;
+	}
+
+	public Server myAlgOld(Job job) throws IOException{
+		
+		send("GETS Capable " + job.getCore() + " " + job.getMemory() + " " + job.getDisk());
+		int serverNum = Integer.parseInt(reader.nextEntry());
+		send("OK");
+		List<Server> servers = new ArrayList<Server>();
+		for (int i = 0; i < serverNum; i++){
+			Server server = new Server(reader);
+			servers.add(server);
+			if(i != serverNum - 1){
+				reader.nextLine();
+			}
+		}
+
+		Server forUse = new Server();
+		Server next = new Server();
+		for (Server s : servers){
+			if (forUse.getType().equals("empty")){
+				forUse = s;
+			} else {
+				next = s;
+				if (forUse.getCore() > next.getCore()){
 					forUse = next;
 				}
 			}
@@ -78,50 +119,6 @@ public class Algorithm {
 			next = s;
 			if (next.getState().equals("idle") && (!forUse.getState().equals("idle") || forUse.getCore() >= next.getCore())){
 				forUse = next;
-			}
-		}
-		send("OK");
-		return forUse;
-	}
-
-	public Server myAlgOld(Job job) throws IOException{
-		
-		send("GETS Avail " + job.getCore() + " " + job.getMemory() + " " + job.getDisk());
-		int serverNum = Integer.parseInt(reader.nextEntry());
-		send("OK");
-		List<Server> servers = new ArrayList<Server>();
-
-		if(serverNum > 0){	
-			for (int i = 0; i < serverNum; i++){
-				Server server = new Server(reader);
-				servers.add(server);
-				if(i != serverNum - 1){
-					reader.nextLine();
-				}
-			}
-		} else {
-			send("GETS Capable " + job.getCore() + " " + job.getMemory() + " " + job.getDisk());
-			serverNum = Integer.parseInt(reader.nextEntry());
-			send("OK");
-			for (int i = 0; i < serverNum; i++){
-				Server server = new Server(reader);
-				servers.add(server);
-				if(i != serverNum - 1){
-					reader.nextLine();
-				}
-			}
-		}
-
-		Server forUse = new Server();
-		Server next = new Server();
-		for (Server s : servers){
-			if (forUse.getType().equals("empty")){
-				forUse = s;
-			} else {
-				next = s;
-				if ((forUse.getCore() > next.getCore()) || forUse.getCore() == next.getCore() && ((forUse.getWJobs() + forUse.getRJobs()) > (next.getWJobs() + next.getRJobs()))){
-					forUse = next;
-				}
 			}
 		}
 		send("OK");
