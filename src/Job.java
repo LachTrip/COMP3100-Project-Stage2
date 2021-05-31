3a06e0527e036ad765eac39313f1ad777859f11e
LachTrip <lachlan.tripolone@students.mq.edu.au>
2021-05-31 15:50:53 +1000

    Moved client files into src folder for cleanliness.

---
+++ src/Job.java
@@ -0,0 +1,45 @@
+public class Job {
+
+    private int submitTime;
+    private int ID;
+    private int estRunTime;
+    private int core;
+    private int memory;
+    private int disk;
+
+    // initialize job with information from ds-server
+    public Job(Reader reader){
+        submitTime = Integer.parseInt(reader.nextEntry());
+        ID = Integer.parseInt(reader.nextEntry());
+        estRunTime = Integer.parseInt(reader.nextEntry());
+        core = Integer.parseInt(reader.nextEntry());
+        memory = Integer.parseInt(reader.nextEntry());
+        disk = Integer.parseInt(reader.nextEntry());
+    }
+
+    //getters
+
+    public int getSubmitTime(){
+        return submitTime;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public int getEstRunTime(){
+        return estRunTime;
+    }
+
+    public int getCore(){
+        return core;
+    }
+
+    public int getMemory(){
+        return memory;
+    }
+
+    public int getDisk(){
+        return disk;
+    }
+}
