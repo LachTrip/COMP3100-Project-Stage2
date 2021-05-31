3e858045cd6b8bd7b40a00a9af8c334aae37dd39
LachTrip <lachlan.tripolone@students.mq.edu.au>
2021-05-31 13:47:20 +1000

    just mucking about

--- Algorithm.java
+++ Algorithm.java
@@ -21,45 +21,7 @@ public class Algorithm {
 		Server next = new Server();
 		int serverNum;
 
-		send("GETS Capable " + job.getCore() + " " + job.getMemory() + " " + job.getDisk());
-		serverNum = Integer.parseInt(reader.nextEntry());
-		send("OK");
-		servers = new ArrayList<Server>();
-			for (int i = 0; i < serverNum; i++){
-			Server server = new Server(reader);
-			servers.add(server);
-			if(i != serverNum - 1){
-				reader.nextLine();
-			}
-		}
-		send("OK");
-
-		//find smallest capable server type
-		for (Server s : servers){
-			if (forUse.getType().equals("empty")){
-				forUse = s;
-			} else {
-				next = s;
-				for(Server t : allServers()){
-					for(Server u : allServers()){
-						if(forUse.getType()==t.getType() && next.getType() == u.getType()){
-							if (t.getCore() > u.getCore()){
-								forUse = next;
-							}
-						}
-					}
-					
-				}
-			}
-		}
-
-		//find server (of smallest capable type) with least number of incomplete jobs
-		for (Server s : servers){
-			next = s;
-			if (forUse.getType() == next.getType() && forUse.getRJobs() + forUse.getWJobs() > next.getRJobs() + next.getWJobs()){
-				forUse = next;
-			}
-		}
+		
 
 		// Server bestOfSmall = forUse;
 		
@@ -120,6 +82,46 @@ public class Algorithm {
 					forUse = next;
 				}
 			}
+		} else {
+			send("GETS Capable " + job.getCore() + " " + job.getMemory() + " " + job.getDisk());
+		serverNum = Integer.parseInt(reader.nextEntry());
+		send("OK");
+		servers = new ArrayList<Server>();
+			for (int i = 0; i < serverNum; i++){
+			Server server = new Server(reader);
+			servers.add(server);
+			if(i != serverNum - 1){
+				reader.nextLine();
+			}
+		}
+		send("OK");
+
+		//find smallest capable server type
+		for (Server s : servers){
+			if (forUse.getType().equals("empty")){
+				forUse = s;
+			} else {
+				next = s;
+				for(Server t : allServers()){
+					for(Server u : allServers()){
+						if(forUse.getType()==t.getType() && next.getType() == u.getType()){
+							if (t.getCore() > u.getCore()){
+								forUse = next;
+							}
+						}
+					}
+					
+				}
+			}
+		}
+
+		//find server (of smallest capable type) with least number of incomplete jobs
+		for (Server s : servers){
+			next = s;
+			if (forUse.getType() == next.getType() && forUse.getRJobs() + forUse.getWJobs() > next.getRJobs() + next.getWJobs()){
+				forUse = next;
+			}
+		}
 		}
 
 		return forUse;
